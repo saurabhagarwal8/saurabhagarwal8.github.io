dbee1e84bdfe681c4f608c97f0b2e1f4681dab87
saurabhagarwal8 <saurabh28a@gmail.com>
2019-03-03 02:49:11 +0530

    initial with home and about

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { RouterModule,Routes } from '@angular/router';
 
-@NgModule({
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+import "materialize-css";
+
+
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+];
+
+@NgModule({ 
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
